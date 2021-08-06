70c4c5458c901c4ee09564e20ee3015dfbcb1778
rkpsx7 <rkpsx7@gmail.com>
2021-08-06 16:35:09 +0530

    added to git

---
+++ app/src/main/java/com/example/thread2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.thread2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+
+public class MainActivity extends AppCompatActivity {
+private CircularProgressIndicator progressBar;
+
+private Handler mainThreadHandler = new Handler(Looper.getMainLooper()){
+    @Override
+    public void handleMessage(@NonNull Message msg) {
+        super.handleMessage(msg);
+        int progress = (int) msg.obj;
+        progressBar.setProgress(progress);
+    }
+};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progressBar);
+        WorkerThread workerThread = new WorkerThread("async",mainThreadHandler);
+        workerThread.start();
+    }
+}
